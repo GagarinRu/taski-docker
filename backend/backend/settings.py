2583e9c6baf0fc26f057059ffe631ffa4cc8cbc1
Evgeniy_Kudryashov <gagarinru84@yandex.ru>
2024-07-28 12:17:07 +0300

    first

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.28.127', '127.0.0.1', 'localhost', 'gagapracticume.ddns.net']
 
@@ -111,8 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# Указываем корневую директорию для сборки статических файлов;
-# в контейнере это будет /app/collected_static
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
